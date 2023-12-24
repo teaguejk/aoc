61dbc09fd1f8111a1d9c5c9fc60dae99e9fdfdfe
Jaracah Teague <jaracahteague@gmail.com>
2023-12-24 00:44:36 -0500

    change up

--- go/day4a.go
+++ go/day4a.go
@@ -10,6 +10,90 @@ import (
 	"strings"
 )
 
+func processSingleCard(line string) int {
+	split := strings.Split(line, ":")
+
+	all := split[1]
+
+	nsplit := strings.Split(all, " | ")
+
+	win := nsplit[0]
+	num := nsplit[1]
+
+	lwin := len(win)
+	lnum := len(num)
+
+	var winning []int
+	var nums []int
+
+	for i := 0; i < lwin; i++ {
+
+		sn := string(win[i])
+
+		if isInt(string(sn)) {
+			if i < lwin-1 && isInt(string(win[i+1])) {
+				concat := sn + string(win[i+1])
+
+				add, err := strconv.Atoi(concat)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				winning = append(winning, add)
+			} else if i > 0 && !isInt(string(win[i-1])) {
+				add, err := strconv.Atoi(sn)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				winning = append(winning, add)
+			}
+		}
+	}
+
+	for i := 0; i < lnum; i++ {
+
+		sn := string(num[i])
+
+		if isInt(string(sn)) {
+			if i < lnum-1 && isInt(string(num[i+1])) {
+				concat := sn + string(num[i+1])
+
+				add, err := strconv.Atoi(concat)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				nums = append(nums, add)
+			} else if i > 0 && !isInt(string(num[i-1])) {
+				add, err := strconv.Atoi(sn)
+
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				nums = append(nums, add)
+			}
+		}
+	}
+
+	score := 0
+
+	for _, n := range nums {
+		if slices.Contains(winning, n) {
+			if score == 0 {
+				score++
+			} else {
+				score *= 2
+			}
+		}
+	}
+	return score
+}
+
 func day4a() {
 	fmt.Println("Day4a")
 
@@ -28,91 +112,7 @@ func day4a() {
 	for scanner.Scan() {
 		line := scanner.Text()
 
-		split := strings.Split(line, ":") // [Card N , All Numbers]
-
-		all := split[1]
-
-		nsplit := strings.Split(all, " | ") // [Winning Numbers, Card Numbers]
-
-		win := nsplit[0]
-		num := nsplit[1]
-
-		var winning []int
-
-		lwin := len(win)
-
-		for i := 0; i < lwin; i++ {
-
-			sn := string(win[i])
-
-			if isInt(string(sn)) {
-				if i < lwin-1 && isInt(string(win[i+1])) {
-					concat := sn + string(win[i+1])
-
-					add, err := strconv.Atoi(concat)
-
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					winning = append(winning, add)
-				} else if i > 0 && !isInt(string(win[i-1])) {
-					add, err := strconv.Atoi(sn)
-
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					winning = append(winning, add)
-				}
-			}
-		}
-
-		fmt.Println(winning)
-
-		var nums []int
-
-		lnum := len(num)
-
-		for i := 0; i < lnum; i++ {
-
-			sn := string(num[i])
-
-			if isInt(string(sn)) {
-				if i < lnum-1 && isInt(string(num[i+1])) {
-					concat := sn + string(num[i+1])
-
-					add, err := strconv.Atoi(concat)
-
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					nums = append(nums, add)
-				} else if i > 0 && !isInt(string(num[i-1])) {
-					add, err := strconv.Atoi(sn)
-
-					if err != nil {
-						log.Fatal(err)
-					}
-
-					nums = append(nums, add)
-				}
-			}
-		}
-
-		score := 0
-
-		for _, n := range nums {
-			if slices.Contains(winning, n) {
-				if score == 0 {
-					score = 1
-				} else {
-					score *= 2
-				}
-			}
-		}
-		sum += score
+		sum += processSingleCard(line)
 	}
 
 	fmt.Println(sum)
