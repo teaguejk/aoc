79d22449ed332f9c66c75f79f3bee46567f5bd72
Jaracah <jaracahteague@gmail.com>
2024-12-06 21:40:46 -0500

    simplify parse function

--- 2024/aoc/src/day01.rs
+++ 2024/aoc/src/day01.rs
@@ -9,22 +9,17 @@ pub fn parse(path: &str) -> Input {
     let mut left: Vec<i32> = Vec::new();
     let mut right: Vec<i32> = Vec::new();
 
-    if let Ok(lines) = read_lines(path) {
-        let vec_lines: Vec<String> = lines
-                .flatten()
-                .collect();
+    let lines = read_lines(path)
+                .expect("error: could not read input file");
 
-        for line in vec_lines {
-            let nums: Vec<i32> = line
-                                .split("   ")
-                                .map(|x| x.parse().unwrap())
-                                .collect();
-            
-            left.push(nums[0]);
-            right.push(nums[1]);
-        }
-    } else {
-        panic!("error: could not read input file");
+    for line in lines.flatten() {
+        let nums: Vec<i32> = line
+                            .split_whitespace()
+                            .map(|x| x.parse().unwrap())
+                            .collect();
+                            
+        left.push(nums[0]);
+        right.push(nums[1]);
     }
     
     (left, right)
