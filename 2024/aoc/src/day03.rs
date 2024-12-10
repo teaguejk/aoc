f46992a228944eba110d28d08302bf93e2e41d0f
Jaracah <jaracahteague@gmail.com>
2024-12-09 23:41:21 -0500

    add day 3.1 w regex

--- 2024/aoc/src/day03.rs
+++ 2024/aoc/src/day03.rs
@@ -1,21 +1,26 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
 pub const INPUT: &str = "./inputs/03.txt";
 
-type Input = Vec<String>;
+type Input = String;
 
 pub fn parse(path: &str) -> Input {
     read_to_string(path)
         .expect("error: could not read input file")
-        .lines()
-        .map(|line| line.to_string())
-        .collect()
 }
 
 pub fn part1(input: &Input) -> i32 {
-    
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut total = 0;
+
+    for group in regex.captures_iter(input) {
+        total += group[1].parse::<i32>().unwrap() * group[2].parse::<i32>().unwrap();
+    }
+
+    total
 }
 
 pub fn part2(input: &Input) -> i32 {
-
+    0
 }
--- 2024/aoc/src/main.rs
+++ 2024/aoc/src/main.rs
@@ -1,6 +1,7 @@
 
 mod day01;
 mod day02;
+mod day03;
 
 fn main() {
     let day1 = || {
@@ -25,8 +26,19 @@ fn main() {
     };
     println!("day 2: {:?}", day2());
 
+    let day3 = || {
+        use day03::*;
+
+        let input = parse(INPUT);
+        let part1 = part1(&input);
+        let part2 = part2(&input);
+
+        (part1.to_string(), part2.to_string())
+    };
+    println!("day 3: {:?}", day3());
+
     // timing
     // let now = Instant::now();
     // let elapsed = now.elapsed();
     // println!("took: {:.2?}", elapsed);
-}
+} 
