e859950dd4f30a327a507ecaf55634b3a66b702d
Jaracah Teague <jaracahteague@gmail.com>
2024-12-30 02:09:45 -0500

    clean up main

--- 2024/aoc/src/main.rs
+++ 2024/aoc/src/main.rs
@@ -6,80 +6,33 @@ mod day03;
 mod day04;
 mod day05;
 
+macro_rules! run_day {
+    ($day:ident) => {
+        (
+            stringify!($day), || {
+                use $day::*;
+                let input = parse(INPUT);
+                let part1 = part1(&input);
+                let part2 = part2(&input);
+                (part1.to_string(), part2.to_string())
+            },
+        )
+    };
+}
+
 fn main() {
-    let day1 = || {
-        use day01::*;
+    let days: Vec<(&str, fn() -> (String, String))> = vec![
+        run_day!(day01),
+        run_day!(day02),
+        run_day!(day03),
+        run_day!(day04),
+        run_day!(day05),
+    ];
 
-        let input = parse(INPUT);
-        let part1 = part1(&input);
-        let part2 = part2(&input);
-
-        (part1.to_string(), part2.to_string())
-    };
-    let now = Instant::now();
-    println!("day 1: {:?}", day1());
-    let elapsed = now.elapsed();
-    println!("took: {:.2?}", elapsed);
-
-    let day2 = || {
-        use day02::*;
-
-        let input = parse(INPUT);
-        let part1 = part1(&input);
-        let part2 = part2(&input);
-
-        (part1.to_string(), part2.to_string())
-    };
-    let now = Instant::now();
-    println!("day 2: {:?}", day2());
-    let elapsed = now.elapsed();
-    println!("took: {:.2?}", elapsed);
-
-    let day3 = || {
-        use day03::*;
-
-        let input = parse(INPUT);
-        let part1 = part1(&input);
-        let part2 = part2(&input);
-
-        (part1.to_string(), part2.to_string())
-    };
-    let now = Instant::now();
-    println!("day 3: {:?}", day3());
-    let elapsed = now.elapsed();
-    println!("took: {:.2?}", elapsed);
-
-    let day4 = || {
-        use day04::*;
-
-        let input = parse(INPUT);
-        let part1 = part1(&input);
-        let part2 = part2(&input);
-
-        (part1.to_string(), part2.to_string())
-    };
-
-    let now = Instant::now();
-    println!("day 4: {:?}", day4());
-    let elapsed = now.elapsed();
-    println!("took: {:.2?}", elapsed);
-
-    let day5 = || {
-        use day05::*;
-
-        let input = parse(INPUT);
-        let part1 = part1(&input);
-        let part2 = part2(&input);
-
-        (part1.to_string(), part2.to_string())
-    };
-
-    let now = Instant::now();
-    println!("day 45 {:?}", day5());
-    let elapsed = now.elapsed();
-    println!("took: {:.2?}", elapsed);
-    // timing
-    // let now = Instant::now();
-    // let elapsed = now.elapsed();
-    // println!("took: {:.2?}", elapsed);
-} 
+    for (day, run) in days {
+        let now = Instant::now();
+        let res = run();
+        let took = now.elapsed();
+        println!("{:<6}: took {:<10.2?} -> {:?}", day, took, res);
+    }
+}
